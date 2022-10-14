da6233d09c6290530d101b9f8654e888d8b853a4
AloGit22 <a3521110093@alumno.uttehuacan.edu.mx>
2022-10-13 20:13:46 -0500

    actualizacion 2

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+      <div class="col-12 col-md-3">
+        <h1>Formularios</h1>
+      </div>
+      <div class="col-12 col-md-6">
+        <img :src="url" :alt="alt" width="100">
+      </div>
+   </div>
+    <hr />
+
+</template>
+
+<script>
+export default {
+  data:() => ( {
+  url:"https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg",
+  alt: "Vue.js", 
+}),
+};
+
+</script>
